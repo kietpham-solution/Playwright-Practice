c3aba1d5cfdac34c04a8813b622c4074ea9dd031
Kiet Pham <kiet.pham@zip.co>
2025-02-16 14:37:08 +1100

    Hooks and Flow Control

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,7 +1,35 @@
 import { test } from '@playwright/test';
 
-test('the first test', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:4200/');
-  await page.getByText('Forms').click();
-  await page.getByText('Form Layouts').click();
+});
+
+test.describe.only('suite1', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText('Forms').click();
+  });
+
+  test('the first test', async ({ page }) => {
+    await page.getByText('Form Layouts').click();
+  })
+
+  test('navigate to datepicker page', async ({ page }) => {
+    await page.getByText('Datepicker').click();
+  });
+
+})
+
+test.describe('suite2', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.getByText('Forms').click();
+  });
+
+  test('the first test', async ({ page }) => {
+    await page.getByText('Form Layouts').click();
+  })
+
+  test('navigate to datepicker page', async ({ page }) => {
+    await page.getByText('Datepicker').click();
+  });
+
 })
